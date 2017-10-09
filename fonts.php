5225e4856692442c455f3e05432232b7f43ae1db
cigreja <cigreja@live.com>
2017-10-09 13:44:51 -0400

    convert html files to php files

---
+++ fonts.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>fonts-template</title>
+
+    <!-- fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+
+
+</head>
+<body>
+
+<p style="font-family: anton;">
+    Anton
+    <br>
+    This is just some random text sample!!
+</p>
+
+<p style="font-family: anton;">
+    Bangers
+    <br>
+    This is just some random text sample!!
+</p>
+
+</body>
+</html>
