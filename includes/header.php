ab9a996ddd2c34403b1ef1cb17c66b983959f791
cigreja <cigreja@live.com>
2017-10-12 13:23:00 -0400

    create new stylesheets for each page.

--- includes/header.php
+++ includes/header.php
@@ -7,20 +7,16 @@
     <meta charset="utf-8">
     <title> Cigreja | Developer </title>
 
-    <!--
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/responsive.css">
-    <meta name="viewport"  content="width=device-width, initial-scale=1.0">
-    -->
-
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="../css/bootswatch/cosmo.css">
 
     <!-- Main CSS -->
+    <link rel="stylesheet" href="../css/home.css">
     <link rel="stylesheet" href="../css/projects.css">
+    <link rel="stylesheet" href="../css/about.css">
+    <link rel="stylesheet" href="../css/contact.css">
 
 </head>
 
--- index.php
+++ index.php
@@ -4,31 +4,43 @@ include 'includes/header.php';
 include 'includes/navigation.php';
 ?>
 
-<section>
-    <h3>Welcome</h3>
+<section id="home-main-section">
 
-    <p>
-        I develop software applications for desktop and mobile devices.
-        I also develop full stack web applications.
-    </p>
+    <!-- Greeting  -->
+    <div id="home-greeting">
+        <h3> Welcome </h3>
+    </div>
 
-    <p>
-        I am currently working as an intern at Elle Investments Inc.
-        I mainly code in Java for client side and server side
-        applications as well as web development.
-    </p>
+    <!-- Introduction  -->
+    <div id="home-intro">
+        <p>
+            I develop software applications for desktop and mobile devices.
+            I also develop full stack web applications.
+        </p>
 
-    <p>
-        I have a lot of projects that I am working on. I will be
-        updating this site with new projects as often as I can.
-        I also post new updates to my facebook and twitter as well.
-        You can follow me and be notified whenever I update a
-        new project.
-    </p>
+        <p>
+            I am currently working as an intern at Elle Investments Inc.
+            I mainly code in Java for client side and server side
+            applications as well as web development.
+        </p>
 
-    <p>
+        <p>
+            I have a lot of projects that I am working on. I will be
+            updating this site with new projects as often as I can.
+            I also post new updates to my facebook and twitter as well.
+            You can follow me and be notified whenever I update a
+            new project.
+        </p>
+    </div>
 
-    </p>
+    <!-- Slider or Feed of new projects from yt fb or twitter  -->
+    <div id="home-not-sure-slider-or-something">
+
+        <p>
+            Going to have my posts or something here !!
+        </p>
+    </div>
+
 </section>
 
 
